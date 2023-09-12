ec99b4f13f687a676437ce60ed853a9b602fa998
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-09-12 17:48:48 -0400

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+window.customerName = 'bob';
+
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+    window.bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = 'some initial value';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'something else';
+}
